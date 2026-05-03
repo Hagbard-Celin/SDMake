6b3e61f8221b1e0e5f0a270d383ff8f9d6fa1564
Hagbard Celine <30884074+Hagbard-Celin@users.noreply.github.com>
2026-05-03 02:29:17 +0200

    Adjust version string.

--- sdmake_rev.h
+++ sdmake_rev.h
@@ -3,4 +3,4 @@
 #define	DATE	"6.2.26"
 #define	VERS	"sdmake 0.1"
 #define	VSTRING	"sdmake 0.1 (6.2.26)\n\r"
-#define	VERSTAG	"\0$VER: sdmake 0.1 (6.2.26)"
+#define	VERSTAG	"\0$VER: sdmake 0.1 (6.2.26) Beta"
